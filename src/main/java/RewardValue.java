229eabc2b1d961acfd2935c27dfad7477e20d27e
kubra <kubracetin.ist@gmail.com>
2023-06-21 13:06:29 -0400

    submit the finished work

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -6,9 +6,20 @@ public class RewardValue {
     //it has two constructors one that accepts a cash value and one that accepts a value in miles.
     public RewardValue(double cash) {
         this.cash = cash;
+        this.valueInMiles = (int) (cash / 0.0035);
     };
 
     public RewardValue(int valueInMiles) {
         this.valueInMiles = valueInMiles;
+        this.cash = valueInMiles * 0.0035;
+    }
+
+    //getCashValue
+    public double getCashValue(){
+        return cash;
+    }
+
+    public int getMilesValue(){
+        return valueInMiles;
     }
 }
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,18 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cash = 100;
+        int expectedResult = (int) (cash / 0.0035);
+        var rewardValue = new RewardValue(cash);
+        assertEquals (expectedResult,rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int miles = 10000;
+        double expectedResult = miles * 0.0035;
+        var rewardValue = new RewardValue(miles);
+        assertEquals(expectedResult, rewardValue.getCashValue());
+
     }
 }
